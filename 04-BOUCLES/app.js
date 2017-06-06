d998c1bdc5d304e351bd632f3741c861a6bc6beb
bethek <kreshnikiberdemaj@gmail.com>
2017-06-06 17:03:54 +0200

    add foler 04-boucles

---
+++ 04-BOUCLES/app.js
@@ -0,0 +1,21 @@
+//Exercices : Boucles
+
+//boucle WHILE
+/*
+var int = 0;
+
+while(int <= 10){
+    console.log(int);
+    //int = int +2;
+    //int += 1;
+    int++;
+
+}
+*/
+
+
+//boucle form-group
+
+for(var int = 0; int < 10; int+=2){
+    console.log(int);
+}
