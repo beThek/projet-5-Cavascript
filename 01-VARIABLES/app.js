d8534dffb9a133c5327a0f0e0b2c8f93f7c43933
bethek <kreshnikiberdemaj@gmail.com>
2017-06-06 12:31:21 +0200

    add Dossier 01

---
+++ 01-VARIABLES/app.js
@@ -0,0 +1,52 @@
+//Exemple :
+/*var bonjour = "Hello world!";
+
+console.log(bonjour);
+*/
+
+/*
+//Exercice : Diseur de bonne aventure
+
+var nombre_denfants = 5;
+var nom_dune_femme = "Justine";
+var nom_dun_pays = "Dubai";
+var métier = "Plombier";
+
+console.log('Vous serez '+métier+' et habiterez à '+nom_dun_pays+ ' et marié à Justine avec '+nombre_denfants+' enfants');
+*/
+
+
+
+/*
+//Exercice : Calculer l'age
+
+var année_courante = 2017
+var mon_année_de_naissance = 1985;
+
+console.log(année_courante+ - +mon_année_de_naissance);
+*/
+
+
+//Exercice : Bientôt vieux
+
+var mon_age = 31;
+var age_max = 100;
+var denree_alimentaire = "feculent";
+var conso_par_jour = 2;
+var nombre_denree = ((conso_par_jour)*(age_max-mon_age))*365;
+
+console.log('Il vous reste '+ nombre_denree+' de '+denree_alimentaire+' avant datteindre lage de ' +age_max);
+
+
+
+/*
+//Exercice : Priorités: pemdas
+
+var result = (1 + 2) * 3 + 4 / 2;
+console.log(result);
+
+console.log('1+2');
+console.log('3*3');
+console.log('4/2');
+console.log('9+2');
+*/
